7d038f286c887ab7cf2be74aface2a68a997ea16
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-11 11:05:15 -0800

    Created User model

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app     = express();
 const PORT    = process.env.PORT || 9000;
-require('./models');
+
+// Require DB and retrieve models
+const db = require('./models');
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`)
