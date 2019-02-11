e5b11fe7c494faf31e680e054be55d2192a98f5c
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-11 11:47:32 -0800

    Set up initial user create route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,4 +8,17 @@ router.get('/', (req, res) => {
   res.send('Reached user index route');
 });
 
+// User Create
+router.post('/', async (req, res) => {
+  try {
+    createdUser = await db.User.create(req.body);
+    console.log(createdUser);
+    res.send('Success')
+
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+})
+
 module.exports = router;
