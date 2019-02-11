d79d6a9b10be2da168df1a77dc36f847fc6831cf
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-11 11:17:22 -0800

    set up express session.

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express    = require('express');
 const app        = express();
 const bodyParser = require('body-parser');
+const session    = require('express-session');
 const PORT       = process.env.PORT || 9000;
 
 // Require DB and retrieve models
 const db = require('./models');
+
+app.use(session({
+  secret: "the doors are closing, please stand clear of the doors",
+  resave: false,
+  saveUninitialized: false
+}))
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
