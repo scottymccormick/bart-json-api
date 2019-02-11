3e598f1d94dac7dada22ab1581f5c916b2b8de7b
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-11 14:59:42 -0800

    specify cors to allow localhost:3000

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(session({
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'http://localhost',
+  origin: 'http://localhost:3000',
   credentials: true,
   optionsSuccessStatus: 200
 }));
