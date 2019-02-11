954ff23fd9998a6f61c8347bbd7705bc0283e4be
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-11 11:08:36 -0800

    Set up body parser

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const app     = express();
-const PORT    = process.env.PORT || 9000;
+const express    = require('express');
+const app        = express();
+const bodyParser = require('body-parser');
+const PORT       = process.env.PORT || 9000;
 
 // Require DB and retrieve models
 const db = require('./models');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`)
