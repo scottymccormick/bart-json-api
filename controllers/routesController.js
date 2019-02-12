be1abc8d5f4b46236cca87baa25053ad7cce8024
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-12 13:42:35 -0800

    add route planner call with origin and destination queries.

--- controllers/routesController.js
+++ controllers/routesController.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router  = express.Router();
+const axios   = require('axios');
 
 router.get('/', (req, res) => {
-  res.send('Reached routes controller root')
+  if (req.query.orig && req.query.dest) {
+    axios.get(`http://api.bart.gov/api/sched.aspx?cmd=depart&orig=${req.query.orig}&dest=${req.query.dest}&json=y&key=${process.env.BART_API_KEY}`)
+      .then((response) => {
+        console.log(response.status);
+        const scheduleInfo = response.data.root.schedule;
+        res.json(scheduleInfo);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  } else {
+    res.send('not enough query parameters');
+  }
 })
 
 module.exports = router;
