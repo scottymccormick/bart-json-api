32eb57f2dbc9d530aaf3ac457ae8b394447505d1
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-12 09:46:28 -0800

    Create stations info route.

--- controllers/stationsController.js
+++ controllers/stationsController.js
@@ -8,26 +8,38 @@ router.get('/', (req, res) => {
     .then((response) => {
       console.log(response.status);
       const stationData = response.data.root.stations.station;
-      console.log(stationData[0])
       const stationList = stationData.map(({name, abbr}) => ({name, abbr}));
       res.json(stationList);
     })
     .catch((err) => {
       res.send(err);
-    })
+    });
 });
 
 // Stations show Estimated Time of Departures
 router.get('/etd/:abbr', (req, res) => {
   axios.get(`http://api.bart.gov/api/etd.aspx?cmd=etd&orig=${req.params.abbr}&json=y&key=${process.env.BART_API_KEY}`)
     .then((response) => {
-      console.log(response)
+      console.log(response.status)
       const stationEtd = response.data.root.station[0];
       res.json(stationEtd.etd);
     })
     .catch((err) => {
       res.send(err);
+    });
+});
+
+// Stations show Station Info
+router.get('/info/:abbr', (req, res) => {
+  axios.get(`http://api.bart.gov/api/stn.aspx?cmd=stninfo&orig=${req.params.abbr}&json=y&key=${process.env.BART_API_KEY}`)
+    .then((response) => {
+      console.log(response.status);
+      const stationInfo = response.data.root.stations.station;
+      res.json(stationInfo);
     })
-})
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
 module.exports = router;
