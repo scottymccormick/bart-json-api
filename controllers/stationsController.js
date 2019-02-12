adc6b792a3f07a47687467dd664000b1731ae747
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-12 09:30:12 -0800

    Stations index with BART API call create.

--- controllers/stationsController.js
+++ controllers/stationsController.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router  = express.Router();
+const axios   = require('axios');
 
+// Stations index
 router.get('/', (req, res) => {
-  res.send('Reached stations root route');
+  axios.get(`http://api.bart.gov/api/stn.aspx?cmd=stns&json=y&key=${process.env.BART_API_KEY}`)
+    .then((response) => {
+      console.log(response.status);
+      const stationData = response.data.root.stations.station;
+      console.log(stationData[0])
+      const stationList = stationData.map(({name, abbr}) => ({name, abbr}));
+      res.json(stationList);
+    })
+    .catch((err) => {
+      res.send(err);
+    })
 })
 
 module.exports = router;
