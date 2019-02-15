231086b74fc9e0286da1a4165541b078cd12b969
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-15 10:15:56 -0800

    Add heroku front end to cors.

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(session({
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ['http://localhost'],
+  origin: ['http://localhost', 'https://barttrack.herokuapp.com/'],
   credentials: true,
   optionsSuccessStatus: 200
 }));
